2a1515a5191d3cc10639d93bdbeabe35f6df713a
Akshat Bhuryan <me@here.com>
2022-10-10 17:15:40 +0530

    variables code added

---
+++ variables/index.js
@@ -0,0 +1,10 @@
+bill=4000;
+min=3999;
+
+if(bill>=min){
+  console.log("The new bill is : "+(bill-(bill*.10)));
+}
+else{
+  console.log("Not eligible");
+}
+
---
+++ variables/problem4.js
@@ -0,0 +1,24 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+var name="Akshat Bhuryan";
+var school="MPS";
+var grade="12th";
+var section="Commerce";
+
+var eco=77;
+var buss=88;
+var acc=99;
+
+console.log("🆁 🅴 🅿 🅾 🆁 🆃 🅲 🅰 🆁 🅳");
+console.log("\n");
+console.log("Name : "+name);
+console.log("School : "+school);
+console.log("Grade : "+grade);
+console.log("Section : "+section);
+console.log("\n");
+console.log("𝕊𝕦𝕓𝕛𝕖𝕔𝕥𝕤");
+console.log("\n");
+console.log("Economics : "+eco);
+console.log("Bussiness : "+ buss);
+console.log("Accounts : "+ acc);
